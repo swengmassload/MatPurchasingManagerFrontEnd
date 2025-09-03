b6b69596d71aeda07d814e8693a1858358fce572
SW ENG <sweng@massload.com>
2025-09-03 08:10:04 -0600

    splitted this test to two small files

--- src/Pages/RMAStages/AssessPackage/Components/AssessPackageAssessmentForm.test.tsx
+++ src/Pages/RMAStages/AssessPackage/Components/AssessPackageAssessmentForm.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen,  } from "@testing-library/react";
 import { describe, it, expect, vi, afterEach } from "vitest";
 import AssessPackageAssessmentForm from "./AssessPackageAssessmentForm";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
